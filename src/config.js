22d015d11919b5ad77f16f481c07e4bfc8c1d1b2
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-10-13 13:49:29 -0700

    change bindingAddress to 0.0.0.0

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const enableWorkers = os.cpus().length !== 1;
 module.exports = {
     //// HOSTING CONFIGURATION ////
 
-    bindingAddress: '127.0.0.1',
+    bindingAddress: '0.0.0.0',
     port: 8080,
     crossDomainPort: 8081,
     publicDir: null, // set to null to disable
